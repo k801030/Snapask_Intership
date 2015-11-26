dc74e592d21a2a791acd745a2a066fee089edffc
Vison <kk801030@gmail.com>
2015-11-26 10:21:12 +0800

    import volley

---
+++ app/src/main/java/project/com/snapask/api/apiService.java
@@ -0,0 +1,8 @@
+package project.com.snapask.api;
+
+/**
+ * Created by Vison on 2015/11/26.
+ */
+public class ApiService {
+
+}
--- app/src/main/java/project/com/snapask/model/UserProfile.java
+++ app/src/main/java/project/com/snapask/model/UserProfile.java
@@ -4,4 +4,22 @@ package project.com.snapask.model;
  * Created by Vison on 2015/11/25.
  */
 public class UserProfile {
+    int id;
+    String email;
+    String username;
+    String profile_pic_file_name;
+
+    String first_name;
+    String last_name;
+    String gender;
+    String phone;
+    String school_name;
+    Subjects subjects;
+
+    class Subjects {
+        int id;
+        String abbr;
+        String description;
+    }
+
 }
